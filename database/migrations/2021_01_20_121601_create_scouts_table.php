6ef95831285c1e515dc14c0767c43699b6cb7d65
dinukolla <dinukolla@.github.com>
2021-01-20 18:15:05 +0530

    Create 2021_01_20_121601_create_scouts_table.php

---
+++ database/migrations/2021_01_20_121601_create_scouts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scouts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scouts');
+    }
+}
