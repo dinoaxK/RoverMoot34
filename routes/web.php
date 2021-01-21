d666fd89e988332b6312fd4577af5ef0bf4eb711
dinukolla <dinukolla@.github.com>
2021-01-22 01:15:37 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::post('logout',[App\Http\Controllers\Auth\LoginController::class,'logout']
 
 Auth::routes(['verify' => true]);
 
-Route::get('/moot/home', [App\Http\Controllers\Portal\Scout\HomeController::class, 'index'])->name('home');
-Route::get('/moot/register', [App\Http\Controllers\Portal\Scout\RegisterController::class, 'index'])->name('register');
-Route::post('/moot/register', [App\Http\Controllers\Portal\Scout\RegisterController::class, 'save_info'])->name('register');
+Route::get('home', [App\Http\Controllers\Portal\Scout\HomeController::class, 'index'])->name('home');
+Route::get('/moot/register', [App\Http\Controllers\Portal\Scout\RegisterController::class, 'index'])->name('moot.register');
+Route::post('/moot/register', [App\Http\Controllers\Portal\Scout\RegisterController::class, 'save_info'])->name('moot.register');
