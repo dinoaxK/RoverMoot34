6c50cfd250f21c52915e2a1f0e4fa2c2da402b7b
dinukolla <dinukolla@.github.com>
2021-01-22 01:15:48 +0530

    save info function

--- app/Http/Controllers/Portal/Scout/RegisterController.php
+++ app/Http/Controllers/Portal/Scout/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Portal\Scout;
 
 use App\Http\Controllers\Controller;
+use App\Models\Participant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
@@ -31,6 +33,7 @@ class RegisterController extends Controller
 
     public function save_info(Request $request)
     {
+        $user_id = Auth::user()->id;
         $validator = Validator::make($request->all(), 
         [            
             'title' => ['nullable', Rule::in(['Mr', 'Master', 'Mrs', 'Miss', 'Dr', 'Rev'])],
@@ -109,9 +112,68 @@ class RegisterController extends Controller
 
         ]);
 
-        if($validator->fails()):
-            return response()->json(['errors'=>$validator->errors()]);
+            //CHECK UNIQUE TYPE AND VALIDATE UNIQUE ID
+        if($request->idType == 'nic'):
+            if(strlen($request->nic)>10):
+            $uniqueID_validator =  Validator::make($request->all(), [
+                'nic' => ['nullable', 'numeric', 'digits:12'],
+            ]);
+            else:
+            $uniqueID_validator =  Validator::make($request->all(), [
+                'nic' => ['nullable', 'alpha_num', 'min:10', 'regex:/^([0-9]{9}[x|X|v|V])$/'],
+            ]);
+            endif;
+        else:
+            $uniqueID_validator =  Validator::make($request->all(), [
+            'unique_id' => ['nullable', 'alpha_num'],
+            ]);
         endif;
-      
+
+        if($validator->fails() || $uniqueID_validator->fails()):
+            return response()->json(['errors'=>$validator->errors()->merge($uniqueID_validator->errors())]);
+        else:
+            if(Participant::where('user_id', $user_id)->first()):
+                $participant = Participant::where('user_id', $user_id)->first();
+            else:
+                $participant = new Participant;
+                $participant->user_id = $user_id;
+            endif;
+            
+            $participant->title = $request->title;
+            $participant->first_name = $request->firstName;
+            $participant->middle_names = $request->middleName;
+            $participant->last_name = $request->lastName;
+            $participant->full_name = $request->fullName;
+            $participant->initials = $request->initials;
+            $participant->dob = $request->dob;
+            $participant->gender = $request->gender;
+            $participant->id_type = $request->idType;
+            $participant->nic = $request->nic;
+            $participant->scout_award = $request->highestScoutAward;
+            $participant->scout_award_date = $request->highestScoutAwardDate;
+            $participant->rover_award = $request->highestRoverAward;
+            $participant->rover_award_date = $request->highestRoverAwardDate;
+            $participant->warrant_number = $request->warrantNumber;
+            $participant->warrant_section = $request->warrantSection;
+            $participant->warrant_expire = $request->warrantValidDate;
+            $participant->crew_number = $request->crewNumber;
+            $participant->crew_district = $request->crewDistrict;
+            $participant->crew_name = $request->crewName;
+            $participant->mobile = $request->mobileNumber;
+            $participant->telephone = $request->telephoneNumber;
+            $participant->address = $request->address;
+            $participant->country = $request->country;
+            $participant->zip = $request->zipCode;
+            $participant->contact_person_title = $request->contactPersontTitle;
+            $participant->contact_person_name = $request->contactPersonName;
+            $participant->contact_person_mobile = $request->contactPersonMobileNumber;
+            $participant->contact_person_telephone = $request->contactPersonTelephoneNumber;
+            $participant->education = $request->educationDetails;
+
+            if($participant->save()):
+                return response()->json(['success'=>'success']);
+            endif;
+        endif;
+        return response()->json(['error'=>'error']);
     }
 }
