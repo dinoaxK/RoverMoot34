b8ef2c1ab87c557b82304efc5b283de739943dcc
dinukolla <dinukolla@.github.com>
2021-01-22 23:57:36 +0530

    Update RegisterController.php

--- app/Http/Controllers/Portal/Scout/RegisterController.php
+++ app/Http/Controllers/Portal/Scout/RegisterController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers\Portal\Scout;
 
 use App\Http\Controllers\Controller;
+use App\Models\Country;
 use App\Models\Participant;
+use App\Models\RoverAward;
+use App\Models\ScoutAward;
+use App\Models\ScoutDistrict;
+use App\Models\Title;
+use App\Models\WarrantSection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -28,7 +34,25 @@ class RegisterController extends Controller
      */
     public function index()
     {
-        return view('portal.scout.register');
+        $participant = NULL;
+        if(Participant::where('user_id', Auth::user()->id)->first()):
+            $participant = Participant::where('user_id', Auth::user()->id)->first();
+        endif;
+        $titles = Title::all();
+        $scouts = ScoutAward::all();
+        $rovers = RoverAward::all();
+        $warrants = WarrantSection::all();
+        $districts = ScoutDistrict::orderBy('name')->get();
+        $countries = Country::orderBy('name')->get();
+        return view ('portal.scout.register', compact(
+            'participant', 
+            'titles', 
+            'scouts', 
+            'rovers',
+            'warrants',
+            'districts',
+            'countries'
+         ));
     }
 
     public function save_info(Request $request)
@@ -36,7 +60,7 @@ class RegisterController extends Controller
         $user_id = Auth::user()->id;
         $validator = Validator::make($request->all(), 
         [            
-            'title' => ['nullable', Rule::in(['Mr', 'Master', 'Mrs', 'Miss', 'Dr', 'Rev'])],
+            'title' => ['nullable', 'exists:titles,name'],
             'firstName' => ['nullable', 'alpha','min:3'],
             'middleName' => ['nullable', 'alpha_dash_space'],
             'lastName' => ['nullable', 'alpha', 'min:3'],
@@ -45,69 +69,33 @@ class RegisterController extends Controller
             'dob' => ['nullable' , 'date','before:today'],
             'gender' => ['nullable', Rule::in(['Male', 'Female', 'Other'])],
             'idType' => ['nullable', Rule::in(['nic', 'passport'])],
+            'citizenship' => ['nullable', Rule::in(['Sri Lankan', 'Foreign National'])],
                   
-            'highestScoutAward' => ['nullable', Rule::in(['Membership','Bronze Star','Silver Star','Gold Star','Scout Award/ Scout Master Award','District Commissioner Cord','Chief Commissioner Award','President Scout Award'])],
+            'highestScoutAward' => ['nullable', 'exists:scout_awards,name'],
             'highestScoutAwardDate' => ['nullable', 'date', 'before:today'],
-            'highestRoverAward' => ['nullable', Rule::in(['Rover Ideals Badge', 'Membership','Invested Rover','Good Citizen Decoration','BP Award','Rover Instructor'])],
+            'highestRoverAward' => ['nullable', 'exists:rover_awards,name'],
             'highestRoverAwardDate' => ['nullable', 'date', 'before:today'],
 
+            'participantType' => ['nullable', Rule::in(['Rover', 'Scout Master'])],
             'warrantNumber' => ['nullable'],
-            'warrantSection' => ['nullable', Rule::in(['Singithi Scout', 'Cub Scout', 'Scout', 'Rover Scout', 'ALT', 'LT'])],
+            'warrantSection' => ['nullable', 'exists:warrant_sections,name'],
             'warrantValidDate' => ['nullable', 'date'],
 
             'crewNumber' => ['nullable', 'integer'],
-            'crewDistrict' => ['nullable', Rule::in([
-                'Akkaraipaththu-Kalmunai',
-                'Ampara',
-                'Anuradhapura',
-                'Avissawella',
-                'Badulla',
-                'Batticaloa',
-                'Chillaw',
-                'Colombo',
-                'Galle',
-                'Gampaha',
-                'Hambanthota',
-                'Homagama',
-                'Jaffna',
-                'Kaluthara',
-                'Kandy',
-                'Kankasanthurai',
-                'Kegalle',
-                'Kilinochchi',
-                'Kurunegala',
-                'Mannar',
-                'Matale',
-                'Matara',
-                'Monaragala',
-                'Moratuwa-Piliyandala',
-                'Mulativu',
-                'Nawalapitiya',
-                'Negambo',
-                'Nuwaraeliya',
-                'Panadura',
-                'Point-Pedro',
-                'Polonnaruwa',
-                'Puttalam',
-                'Rathnapura',
-                'Trincomalee',
-                'Vavuniya',
-                'Wattala-Jaela',
-                'Wennappuwa'
-            ])],
+            'crewDistrict' => ['nullable', 'exists:scout_districts,name'],
             'crewName'=> ['nullable', 'alpha_dash_space', 'min:3'],
             'mobileNumber'=>['nullable', 'numeric', 'digits_between:8,15'],
             'telephoneNumber'=>['nullable', 'numeric', 'digits_between:8,15'],            
             'address' => ['nullable', 'address'],
-            'country' => ['nullable', 'exists:world_countries,name'],
+            'country' => ['nullable', 'exists:countries,name'],
             'zipCode' => ['nullable', 'numeric', 'digits_between:5,10'],
             
-            'contactPersontTitle' => ['nullable', Rule::in(['Mr', 'Master', 'Mrs', 'Miss', 'Dr', 'Rev'])],
+            'contactPersontTitle' => ['nullable', 'exists:titles,name'],
             'contactPersonName' => ['nullable', 'alpha_dash_space'],
             'contactPersonMobileNumber'=>['nullable', 'numeric', 'digits_between:8,15'],
             'contactPersonTelephoneNumber'=>['nullable', 'numeric', 'digits_between:8,15'],
 
-            'educationDetails' => ['nullable', 'regex:/^[a-zA-Z\s]*$/', 'min:3'],
+            'educationDetails' => ['nullable', 'min:3'],
 
 
         ]);
@@ -116,21 +104,37 @@ class RegisterController extends Controller
         if($request->idType == 'nic'):
             if(strlen($request->nic)>10):
             $uniqueID_validator =  Validator::make($request->all(), [
-                'nic' => ['nullable', 'numeric', 'digits:12'],
+                'number' => ['nullable', 'numeric', 'digits:12'],
             ]);
             else:
             $uniqueID_validator =  Validator::make($request->all(), [
-                'nic' => ['nullable', 'alpha_num', 'min:10', 'regex:/^([0-9]{9}[x|X|v|V])$/'],
+                'number' => ['nullable', 'alpha_num', 'min:10', 'regex:/^([0-9]{9}[x|X|v|V])$/'],
             ]);
             endif;
         else:
             $uniqueID_validator =  Validator::make($request->all(), [
-            'unique_id' => ['nullable', 'alpha_num'],
+            'number' => ['nullable', 'alpha_num', 'max:9'],
             ]);
         endif;
 
-        if($validator->fails() || $uniqueID_validator->fails()):
-            return response()->json(['errors'=>$validator->errors()->merge($uniqueID_validator->errors())]);
+        //CHECK PARTICIPANT TYPE AND VALIDATE WARRANT INFO
+        if($request->participantType == 'Rover'):
+            $warrant_validator =  Validator::make($request->all(), [
+                'warrantNumber' => ['nullable'],
+                'warrantSection' => ['nullable', 'exists:warrant_sections,name'],
+                'warrantValidDate' => ['nullable', 'date'],
+            ]);
+        else:
+            $warrant_validator =  Validator::make($request->all(), [
+                'warrantNumber' => ['nullable'],
+                'warrantSection' => ['nullable', 'exists:warrant_sections,name'],
+                'warrantValidDate' => ['nullable', 'date'],
+            ]);
+        endif;
+
+
+        if($validator->fails() || $uniqueID_validator->fails() || $warrant_validator->fails()):
+            return response()->json(['errors'=>$validator->errors()->merge($uniqueID_validator->errors(),$warrant_validator->errors())]);
         else:
             if(Participant::where('user_id', $user_id)->first()):
                 $participant = Participant::where('user_id', $user_id)->first();
@@ -147,12 +151,135 @@ class RegisterController extends Controller
             $participant->initials = $request->initials;
             $participant->dob = $request->dob;
             $participant->gender = $request->gender;
+            $participant->citizenship = $request->citizenship;
             $participant->id_type = $request->idType;
-            $participant->nic = $request->nic;
+            $participant->number = $request->number;
             $participant->scout_award = $request->highestScoutAward;
             $participant->scout_award_date = $request->highestScoutAwardDate;
             $participant->rover_award = $request->highestRoverAward;
             $participant->rover_award_date = $request->highestRoverAwardDate;
+            
+            $participant->participant_type = $request->participantType;
+            $participant->warrant_number = $request->warrantNumber;
+            $participant->warrant_section = $request->warrantSection;
+            $participant->warrant_expire = $request->warrantValidDate;
+            $participant->crew_number = $request->crewNumber;
+            $participant->crew_district = $request->crewDistrict;
+            $participant->crew_name = $request->crewName;
+            $participant->mobile = $request->mobileNumber;
+            $participant->telephone = $request->telephoneNumber;
+            $participant->address = $request->address;
+            $participant->country = $request->country;
+            $participant->zip = $request->zipCode;
+            $participant->contact_person_title = $request->contactPersontTitle;
+            $participant->contact_person_name = $request->contactPersonName;
+            $participant->contact_person_mobile = $request->contactPersonMobileNumber;
+            $participant->contact_person_telephone = $request->contactPersonTelephoneNumber;
+            $participant->education = $request->educationDetails;
+
+            if($participant->save()):
+                return response()->json(['success'=>'success']);
+            endif;
+        endif;
+        return response()->json(['error'=>'error']);
+    }
+
+    public function submit_application(Request $request)
+    {
+        $user_id = Auth::user()->id;
+        $validator = Validator::make($request->all(), 
+        [            
+            'title' => ['required', 'exists:titles,name'],
+            'firstName' => ['required', 'alpha','min:3'],
+            'middleName' => ['nullable', 'alpha_dash_space'],
+            'lastName' => ['required', 'alpha', 'min:3'],
+            'fullName' => ['required', 'alpha_dash_space'],
+            'initials' => ['required', 'alpha_capital'],
+            'dob' => ['required' , 'date','before:today'],
+            'gender' => ['required', Rule::in(['Male', 'Female', 'Other'])],
+            'idType' => ['required', Rule::in(['nic', 'passport'])],
+            'citizenship' => ['required', Rule::in(['Sri Lankan', 'Foreign National'])],
+                  
+            'highestScoutAward' => ['nullable', 'exists:scout_awards,name'],
+            'highestScoutAwardDate' => ['nullable', 'date', 'before:today'],
+            'highestRoverAward' => ['required', 'exists:rover_awards,name'],
+            'highestRoverAwardDate' => ['required', 'date', 'before:today'],
+
+            'participantType' => ['required', Rule::in(['Rover', 'Scout Master'])],
+
+            'crewNumber' => ['required', 'integer'],
+            'crewDistrict' => ['required', 'exists:scout_districts,name'],
+            'crewName'=> ['required', 'alpha_dash_space', 'min:3'],
+            'mobileNumber'=>['required', 'numeric', 'digits_between:8,15'],
+            'telephoneNumber'=>['required', 'numeric', 'digits_between:8,15'],            
+            'address' => ['required', 'address'],
+            'country' => ['required', 'exists:countries,name'],
+            'zipCode' => ['nullable', 'numeric', 'digits_between:5,10'],
+            
+            'contactPersontTitle' => ['required', 'exists:titles,name'],
+            'contactPersonName' => ['required', 'alpha_dash_space'],
+            'contactPersonMobileNumber'=>['required', 'numeric', 'digits_between:8,15'],
+            'contactPersonTelephoneNumber'=>['nullable', 'numeric', 'digits_between:8,15'],
+
+            'educationDetails' => ['nullable', 'min:3'],
+
+
+        ]);
+
+        //CHECK UNIQUE TYPE AND VALIDATE UNIQUE ID
+        if($request->idType == 'nic'):
+            if(strlen($request->nic)>10):
+            $uniqueID_validator =  Validator::make($request->all(), [
+                'number' => ['required', 'numeric', 'digits:12'],
+            ]);
+            else:
+            $uniqueID_validator =  Validator::make($request->all(), [
+                'number' => ['required', 'alpha_num', 'min:10', 'regex:/^([0-9]{9}[x|X|v|V])$/'],
+            ]);
+            endif;
+        else:
+            $uniqueID_validator =  Validator::make($request->all(), [
+                'number' => ['nullable', 'alpha_num', 'max:9' ],
+            ]);
+        endif;            
+
+        //CHECK PARTICIPANT TYPE AND VALIDATE WARRANT INFO
+        if($request->participantType == 'Rover'):
+            $warrant_validator =  Validator::make($request->all(), [
+                'warrantNumber' => ['nullable'],
+                'warrantSection' => ['nullable', 'exists:warrant_sections,name'],
+                'warrantValidDate' => ['nullable', 'date'],
+            ]);
+        else:
+            $warrant_validator =  Validator::make($request->all(), [
+                'warrantNumber' => ['required'],
+                'warrantSection' => ['required', 'exists:warrant_sections,name'],
+                'warrantValidDate' => ['required', 'date'],
+            ]);
+        endif;
+
+        if($validator->fails() || $uniqueID_validator->fails() || $warrant_validator->fails()):
+            return response()->json(['errors'=>$validator->errors()->merge($uniqueID_validator->errors())->merge($warrant_validator->errors())]);
+        else:
+            $participant = Participant::where('user_id', $user_id)->first();
+            
+            $participant->title = $request->title;
+            $participant->first_name = $request->firstName;
+            $participant->middle_names = $request->middleName;
+            $participant->last_name = $request->lastName;
+            $participant->full_name = $request->fullName;
+            $participant->initials = $request->initials;
+            $participant->dob = $request->dob;
+            $participant->gender = $request->gender;
+            $participant->citizenship = $request->citizenship;
+            $participant->id_type = $request->idType;
+            $participant->number = $request->number;
+            $participant->scout_award = $request->highestScoutAward;
+            $participant->scout_award_date = $request->highestScoutAwardDate;
+            $participant->rover_award = $request->highestRoverAward;
+            $participant->rover_award_date = $request->highestRoverAwardDate;
+
+            $participant->participant_type = $request->participantType;
             $participant->warrant_number = $request->warrantNumber;
             $participant->warrant_section = $request->warrantSection;
             $participant->warrant_expire = $request->warrantValidDate;
