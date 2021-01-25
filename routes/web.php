f6adcda2ee04214564a3ba7655600939bb3e2bcf
dinukolla <dinukolla@.github.com>
2021-01-25 17:40:23 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,22 @@ Route::get('/', function () {
 Route::post('logout',[App\Http\Controllers\Auth\LoginController::class,'logout']);
 
 Auth::routes(['verify' => true]);
+Route::post('/portal/update/password', [App\Http\Controllers\Portal\ProfileController::class, 'changePassword'])->name('change.password');
+
 
 // ADMIN PORTAL
 Route::get('/admin/home', [App\Http\Controllers\Portal\Admin\HomeController::class, 'index'])->name('admin.home');
 Route::get('/admin/register', [App\Http\Controllers\Portal\Admin\RegisterController::class, 'index'])->name('admin.register');
+Route::get('/admin/register/list', [App\Http\Controllers\Portal\Admin\RegisterController::class, 'get_participants_list'])->name('paricipant.list');
+Route::get('/admin/participant/profile/{id}/id', [App\Http\Controllers\Portal\Admin\ProfileController::class, 'index'])->name('participant.profile.admin');
+Route::get('/admin/participant/application/{id}/id', [App\Http\Controllers\Portal\Admin\ProfileController::class, 'application'])->name('participant.application.admin');
+Route::get('/admin/participant/payment/{id}/id', [App\Http\Controllers\Portal\Admin\ProfileController::class, 'payment'])->name('participant.payment.admin');
+
+Route::post('/admin/register/approve/application', [App\Http\Controllers\Portal\Admin\PofileController::class, 'approve_application'])->name('approve.application');
+Route::post('/admin/register/decline/application', [App\Http\Controllers\Portal\Admin\PofileController::class, 'decline_application'])->name('decline.application');
+
+Route::post('/admin/register/approve/payment', [App\Http\Controllers\Portal\Admin\PofileController::class, 'approve_payment'])->name('approve.payment');
+Route::post('/admin/register/decline/payment', [App\Http\Controllers\Portal\Admin\PofileController::class, 'decline_payment'])->name('decline.payment');
 
 // /ADMIN PORTAL
 
