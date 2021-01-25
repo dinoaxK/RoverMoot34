17f0a2304db45f351b02743dfd5dbd886f088856
dinukolla <dinukolla@.github.com>
2021-01-25 17:41:10 +0530

    Update scripts.blade.php

--- resources/views/portal/scout/application/scripts.blade.php
+++ resources/views/portal/scout/application/scripts.blade.php
@@ -2,6 +2,8 @@
 <script type="text/javascript">
 
 $(document).ready(function(){
+  
+  document.title = "{{ $participant->full_name }}";
   $('body').css('background-color', '#fff');
   $('footer').addClass('d-none');
   $('nav').addClass('d-none');
