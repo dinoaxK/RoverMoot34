c218245d6fab72b3ef956f6d475fddef01964ddb
dinukolla <dinukolla@.github.com>
2021-01-25 17:43:12 +0530

    Update register.blade.php

--- resources/views/portal/admin/register.blade.php
+++ resources/views/portal/admin/register.blade.php
@@ -3,28 +3,43 @@
 @section('content')
 
 
-<div class="container min-vh-100 mb-5 pt-5">
-    <div class="row justify-content-center pt-5">
+<div class="container  min-vh-100 my-5 pt-5">
+    <div class="row pt-5">
     <nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item active" aria-current="page">Home</li>
+    <ol class="breadcrumb">    
+        <li class="breadcrumb-item"><a class="mb-0" href="{{ route('admin.home') }}">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Registration</li>
     </ol>
     </nav>
-        <div class="col-lg-10 mt-5">
+        <div class="col-lg-12  mx-0 mt-5">
             <div class="py-5">
-                @if(Auth::user()->participant && Auth::user()->participant->application_status == 1)
-
-                @else
-                    <div class="alert alert-warning" role="alert">
-                        <h2 class="alert-heading">Registration Under Construction!</h4>
-                        <p>Registration will be available as soon!! </p>
-                        <p class="mb-0">Stay tuned for the First Ever Virtual National Rover Scout Moot...</p>
-                    </div>          
-                @endif
+                <div class="container px-0 mt-5">
+                    <h2 class="mb-4 text-dark">Participant Registrations</h2>
+                    <table class="table table-bordered yajra-datatable text-center">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>District</th>
+                                <th>Category</th>
+                                <th>Name</th>
+                                <th>Gender</th>
+                                <th>Mobile</th>
+                                <th>Residence</th>
+                                <th>Payment Ref</th>
+                                <th>Payment</th>
+                                <th>Application</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 
+@include('portal.admin.register.scripts')
 @endsection
