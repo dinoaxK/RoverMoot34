1aebadd2d946c288d608611561548cef98084612
dinukolla <dinukolla@.github.com>
2021-01-28 18:26:10 +0530

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->truncate();
         DB::table('users')->insert(
             array(
                 ['name'=> 'Dinusha Kulasooriya',
