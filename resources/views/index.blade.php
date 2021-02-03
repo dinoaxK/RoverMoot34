ae289c061e5e1d1e1e5f65214f77c447b941b895
dinukolla <dinukolla@.github.com>
2021-02-03 17:45:39 +0530

    about updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,10 +23,19 @@
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>About the Moot</h2>
-          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus sequi voluptatum quidem optio! Nisi assumenda illum, facilis doloremque quam obcaecati magnam, consequatur vel beatae totam tempore, vitae ducimus a autem.</p>
+          <p class="lead">34th National Rover Moot- the biannual gathering of Rover Scouts of Sri Lanka Scout Association, will be held virtually from 25th to 28th of March 2021 via Zoom, Facebook & YouTube. Young scouts between the age of 17 to 26 years are eligible to participate in this camp.</p>
+          <p class="lead">Rover scouting was initiated in Sri Lanka in 1920. Since its inception, Rover scouting has grown up to a larger platform which gathers the youth of the country, mentoring  them to lead a successful adult life utilising the guidance and the philosophy for a thriving adulthood mentioned in "Rovering to Success"  the life-guide book for Rovers written by Baden-Powell himself..</p>
+          <p class="lead">While celebrating  the Rover Centenary, the 34th National Rover Moot will provide  an opportunity to the creative young Rovers to take part in the Moot logo and theme competition from which an official logo and a theme for the Moot will be selected. The competition will be launched on the [date]  and we invite all the talented Rovers out there to take part in this as an initial step of celebrating the centenary.</p>
+         
           
         </div>
       </div>
+    </div>
+  </section>
+
+  <section id="joinus" class="bg-light text-dark" style="background-image: url({{ asset('img/background/1.jpg') }});">
+    <div class="container my-0 py-0" >
+          <h5 class="text-center hero-header" style="font-size: 20px;">Join hands with us today to celebrate the service, unity and fellowship of 100 years!</h1>
     </div>
   </section>
 
@@ -65,31 +74,31 @@
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>Register</h2>
-          <p>To join with the Virtual National Rover Scout Moot, You need to follow these steps.</p>
+          <p>To join the Virtual National Rover Scout Moot, you need to follow these steps.</p>
           <ul>
             <li>
               <p>
-                Step 01: Sign up and create your account with your email and password (You need to verify your email to complete signup)
+                Step 01: Sign up and create an account with your email and password (You need to verify your email to complete signup)
               </p>
             </li>
             <li>
               <p>
-                Step 02: Complete your Rover Moot Registration Payment to the below account and  
+                Step 02: Complete your Rover Moot Registration Payment to the account below and snap a photo of the payment proof (e.g. Bank Slip) 
               </p>
             </li>
             <li>
               <p>
-                Step 03: Go to Register and fill out the Rover Moot Application providing all the required details including the photo of your paid bank slip or receipt. (You can save your details and submit your application later, your details will be editable until you submit)
+                Step 03: Go to Register and fill out the Rover Moot Application providing all the required details including the photo of your paid bank slip or receipt. (You can save your details and submit your application later. your details will be editable until you submit)
               </p>
             </li>
             <li>
               <p>
-                Step 04: After submitting the application, Print the auto generated application and get it verified by your Rover Scout Master, Your District's Asst. District Commissioner (Rovers) And District Commissioner with their relevant stamps.
+                Step 04: After submitting the application, print the auto generated application and get it verified by your Rover Scout Master, Your District's Asst. District Commissioner (Rovers) And District Commissioner with their relevant stamps.
               </p>
             </li>
             <li>
               <p>
-                Step 05: Go to "Register" and Upload a clear photo of your signed application.
+                Step 05: Go to "Register" and upload a clear photo of your signed application.
               </p>
             </li>
           </ul>
