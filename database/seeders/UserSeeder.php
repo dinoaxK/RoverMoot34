2b506760e5e05ac88b3bd8347e3ed9670a5cef79
dinukolla <dinukolla@.github.com>
2021-03-01 12:44:59 +0530

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,23 +23,7 @@ class UserSeeder extends Seeder
                 'role'=> 'super_admin',
                 'email_verified_at'=> '2021-01-24 17:37:43',
                 'created_at'=> '2020-11-23 10:13:53',
-                'updated_at'=> '2020-11-23 10:13:53'],
-
-                ['name'=> 'Janith Pathirana',
-                'email'=>"avishka.janith@gmail.com",
-                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG',
-                'role'=> 'admin',
-                'email_verified_at'=> '2021-01-24 17:37:43',
-                'created_at'=> '2020-11-23 10:13:53',
-                'updated_at'=> '2020-11-23 10:13:53'],
-
-                ['name'=> 'Gimhani Rajapaksha',
-                'email'=>"gimhaniarajapaksha@gmail.com",
-                'password'=>'$2y$10$chLDig2zqSahPLNRnur.0O3xzyvjlzH54Jwsp.gblzj3Ltp.dfxkG',
-                'role'=> 'scout',
-                'email_verified_at'=> '2021-01-24 17:37:43',
-                'created_at'=> '2020-11-23 10:13:53',
-                'updated_at'=> '2020-11-23 10:13:53'],
+                'updated_at'=> '2020-11-23 10:13:53']
             )
         );
     }
