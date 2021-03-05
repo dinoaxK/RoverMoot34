96810071cc3d7a10371f4885ca2018d576b268cc
dinukolla <dinukolla@.github.com>
2021-03-05 14:17:12 +0530

    Update modals.blade.php

--- resources/views/portal/admin/register/modals.blade.php
+++ resources/views/portal/admin/register/modals.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Application Proof</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="location.reload()">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
@@ -42,7 +42,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Application Proof</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="location.reload()">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
