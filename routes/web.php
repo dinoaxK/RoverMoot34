e20048f1c2ebdafd7da7fac69dcf7fa6b9615d7c
dinukolla <dinukolla@.github.com>
2021-04-07 17:21:33 +0530

    addchat plugin

--- routes/web.php
+++ routes/web.php
@@ -81,3 +81,5 @@ Route::post('/moot/register/scanned', [App\Http\Controllers\Portal\Scout\Registe
 
 Route::get('/moot/register/print/application', [App\Http\Controllers\Portal\Scout\PrintController::class, 'index'])->name('moot.application.print');
 // SCOUT PORTAL
+
+Addchat::routes();
