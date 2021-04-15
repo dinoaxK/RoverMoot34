bb55831802b90aaf33d8e0323d9fba024b867d2c
dinukolla <dinukolla@.github.com>
2021-04-15 21:32:36 +0530

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,6 +1,6 @@
 <tr>
 <td class="header">
-<a href="{{ url('/') }}" style="width:100%;">
+<a href="https://rovermoot.scout.lk" style="width:100%;">
 @if (trim($slot) === '34th National Moot Web')
 <img src="https://rovermoot.scout.lk/img/background/email%20banner2.jpg" class="logo" alt="Moot Email Banner">
 @else
