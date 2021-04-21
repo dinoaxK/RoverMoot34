33a6484602b94a137fa26d5aa03e4c0769774bdd
dinukolla <dinukolla@.github.com>
2021-04-21 13:15:24 +0530

    Update ChatController.php

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -7,6 +7,7 @@ use App\Models\LoginDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 use function PHPUnit\Framework\isEmpty;
 
@@ -54,13 +55,51 @@ class ChatController extends Controller
     {
 
         if (
-            preg_match('/\bfuck\b/', $request->message) ||
-            preg_match('/\bsuck\b/', $request->message) ||
-            preg_match('/\bdick\b/', $request->message) ||
-            preg_match('/\bcock\b/', $request->message) 
-            ) {
+            preg_match('/fuck/', strtolower($request->message)) ||
+            preg_match('/punda/', strtolower($request->message)) ||
+            preg_match('/kotte/', strtolower($request->message)) ||
+            preg_match('/oka/', strtolower($request->message)) ||
+            preg_match('/okka/', strtolower($request->message)) ||
+            preg_match('/suththukudi/', strtolower($request->message)) ||
+            preg_match('/suttukudi/', strtolower($request->message)) ||
+            preg_match('/kallawettama/', strtolower($request->message)) ||
+            preg_match('/kalla/', strtolower($request->message)) ||
+            preg_match('/wettama/', strtolower($request->message)) ||
+            preg_match('/suck/', strtolower($request->message)) ||
+            preg_match('/dick/', strtolower($request->message)) ||
+            preg_match('/pussy/', strtolower($request->message)) ||
+            preg_match('/ass/', strtolower($request->message)) ||
+            preg_match('/masturbate/', strtolower($request->message)) ||
+            preg_match('/masturbation/', strtolower($request->message)) ||
+            preg_match('/cock/', strtolower($request->message)) ||
+            preg_match('/boobs/', strtolower($request->message)) ||
+            preg_match('/boob/', strtolower($request->message)) ||
+            preg_match('/horny/', strtolower($request->message)) ||
+            preg_match('/pee/', strtolower($request->message)) ||
+            preg_match('/fucker/', strtolower($request->message)) ||
+            preg_match('/huth/', strtolower($request->message)) ||
+            preg_match('/paka/', strtolower($request->message)) ||
+            preg_match('/kari/', strtolower($request->message)) ||
+            preg_match('/wes/', strtolower($request->message)) ||
+            preg_match('/hukan/', strtolower($request->message)) ||
+            preg_match('/kimb/', strtolower($request->message)) ||
+            preg_match('/puka/', strtolower($request->message)) ||
+            preg_match('/walla/', strtolower($request->message)) ||
+            preg_match('/paiya/', strtolower($request->message)) ||
+            preg_match('/ponna/', strtolower($request->message)) 
+            ):
             return 'bad words';
-        }
+        endif;
+
+        $validator = Validator::make($request->all(), [
+            'message'=>['required']
+        ]);
+
+        if($validator->fails()):
+            return response()->json(['errors'=>$validator->errors()]);
+        endif;
+
+
         $message = new ChatMessage();
 
         $message->user_id=Auth::user()->id;
